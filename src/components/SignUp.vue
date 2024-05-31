ec3f00f56e28a095f35bc109726b7301bfef5bf9
shobhit shukla <shobhit.ssic@gmail.com>
2024-05-31 17:42:51 +0530

    first module
    json server setup
    test api on postman api
    installation and axios package for API
    call API
    store Data in local storage

---
+++ src/components/SignUp.vue
@@ -0,0 +1,64 @@
+<template>
+    <img class="logo" alt="Aviral Restro Logo" src="../assets/logo2.png">
+    <h1> Sign UP </h1>
+    <div class="signup">
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <input type="text" v-model="email" placeholder="Enter Email ID"/>
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="signUp">Sign Up</button>
+    </div>
+</template>
+<script>
+
+import axios from 'axios'
+export default {
+    name : 'SignUp',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp(){
+            {
+                let result = await axios.post("http://localhost:3000/user",{
+                    email:this.email,
+                    name: this.name,
+                    password: this.password
+                });
+                console.warn(result);
+                if(result.status==201){
+                    alert("Sign-up done")
+                    localStorage.setItem("user-info",JSON.stringify(result.data))
+                }
+            }
+        }
+    }
+}
+</script>
+<style>
+.logo{
+    width: 100px;
+}
+.signup input{
+    width: 300px;
+    height: 30px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 2px solid black;
+}
+.signup button{
+    color: white;
+    background-color: black;
+    margin: 1%;
+    width: 300px;
+    height: 30px;
+    border-radius: 15px;
+    cursor: pointer;
+}
+</style>
